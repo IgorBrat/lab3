8f381cacf61bc4170957483d072cf7dbced6a6d7
IgorBrat <91553548+IgorBrat@users.noreply.github.com>
2021-12-20 12:42:13 +0200

    Update lab3.c
    Changed name variable "s" to "input_string" in lines 5-13
    Changed string length on line 19 to const variable LEN=100 (defined in line 3)

--- lab3.c
+++ lab3.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#define LEN 100
 
 void reverseString(char input_string[])     // function which reverses inputed string
 {
 	char* temp;
-	for (int i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	for (int i = 0, j = strlen(input_string) - 1; i < j; i++, j--)
 	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
+		temp = input_string[i];
+		input_string[i] = input_string[j];
+		input_string[j] = temp;
 	}
 }
 
 void main(void)
 {
 	printf("Input string: ");
-	char str[100];
+	char str[LEN];
 	gets(str);
 	reverseString(str);
 	printf("Reversed string: %s", str);
